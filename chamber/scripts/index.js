a044df1ff59e019b91d455bd03b702a262fb1530
AndrewPoore <andrewpoore27@gmail.com>
2022-10-08 16:26:11 -0600

    portal update

---
+++ chamber/scripts/index.js
@@ -0,0 +1,40 @@
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let months = [
+    "January",
+    "Februray",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d = new Date();
+
+let dayName = days[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = d.getFullYear();
+
+let fullDate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
+
+document.querySelector('#year').innerHTML = year;
+
+document.querySelector('#currentDate').innerHTML = fullDate;
+
+document.querySelector(
+	"#lastModified"
+).textContent = `Last Modification: ${document.lastModified}`;
